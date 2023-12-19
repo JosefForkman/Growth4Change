08b106bbf8c3ce7e6b7cdcb10d131e9716e17984
MagnusVV <112693199+MagnusVV@users.noreply.github.com>
2023-12-19 09:07:38 +0100

    Update header.blade.php
    Fixed sloppy typed path in navbar.

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,9 +1,9 @@
 <?php
 
 $navBar = '';
-$navBarInner='';
-$navList='';
-$navListItem='';
+$navBarInner = '';
+$navList = '';
+$navListItem = '';
 
 ?>
 
@@ -11,11 +11,12 @@ $navListItem='';
     <div class={{$navBarInner}}>
         <ul class={{$navList}}>
             <li class={{$navListItem}}><a href='/'>ABOUT</a></li>
-            <li class={{$navListItem}}><a href='ecosystem'>ECOSYSTEM</a></li>
-            <li class={{$navListItem}}><a href='food-is-politics'>FOOD IS POLITICS</a></li>
-            <li class={{$navListItem}}><a href='resources'>RESOURCES</a></li>
-            <li class={{$navListItem}}><a href='grow-with-us'>GROW WITH US</a></li>
+            <li class={{$navListItem}}><a href='/ecosystem'>ECOSYSTEM</a></li>
+            <li class={{$navListItem}}><a href='/food-is-politics'>FOOD IS POLITICS</a></li>
+            <li class={{$navListItem}}><a href='/resources'>RESOURCES</a></li>
+            <li class={{$navListItem}}><a href='/grow-with-us'>GROW WITH US</a></li>
         </ul>
     </div>
 </div>
+
 
