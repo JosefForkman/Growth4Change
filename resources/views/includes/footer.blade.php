20b52bcb1310252b13971e166eacbd0a41e9d65e
JosefForkman <strand.vatten@outlook.com>
2023-12-21 10:03:47 +0100

    finished footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,31 @@
-<footer class="grid grid-cols-1 justify-items-start  gap-y-10 text-white bg-growthdarkgrey pt-8 px-16 pb-10">
-    <p class="font-body text-lg md:text-xl xl:text-2xl max-w-md">Growth4change unites changemakers to ghetto engineer innvative solutions for urban transformation.</p>
+<footer
+    class="
+        grid
+        grid-cols-1
+        md:grid-cols-3
+        justify-items-center
+        md:justify-items-start
+        md:items-end
+        gap-y-10
+        md:gap-x-24
+        text-white
+        bg-growthdarkgrey
+        pt-8
+        px-4
+        md:px-10
+        pb-10">
+
+    <p class="
+        font-body
+        text-lg
+        md:text-xl
+        xl:text-2xl
+        max-w-md
+        md:max-w-none
+        md:col-span-full
+        xl:col-span-1">
+        Growth4change unites changemakers to ghetto engineer innvative solutions for urban transformation.
+    </p>
 
     <div class="grid grid-cols-2">
         <h2 class="font-heading text-2xl col-span-full md:text-3xl xl:text-4xl">Follow US</h2>
@@ -11,7 +37,7 @@
         </a>
     </div>
 
-    <div class="md:col-start-2 md:row-start-2">
+    <div class="flex flex-col items-center md:items-start xl:col-start-2">
         <h2 class="font-heading text-2xl md:text-3xl xl:text-4xl">Contact Us</h2>
         <a href="mailto:info@growth4change.net" class="font-body text-lg md:text-xl xl:text-2xl">info@growth4change.net</a>
     </div>
@@ -19,6 +45,6 @@
     <img
         src='assets/Resurs-1-8.png'
         alt="Growth4change logo"
-        class="md:row-span-2 w-64">
+        class="w-64 md:justify-self-end xl:row-span-2 xl:row-start-1 xl:col-start-3">
     <p class="font-body text-lg md:text-xl xl:text-2xl text-growthlightgrey md:col-span-3 justify-self-center">© Growth4change Est. 2019. All Rights to Reserved.</p>
 </footer>
