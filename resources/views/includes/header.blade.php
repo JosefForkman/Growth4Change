419bdd50dbaacd729df2203a8f5e3e1c4bc43cd0
MagnusVV <112693199+MagnusVV@users.noreply.github.com>
2023-12-21 10:55:08 +0100

    Fixed logo size in header.

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,6 +1,6 @@
 <div class='bg-growthdarkgrey w-full p-4 md:px-12 md:py-4 xl:px-16 xl:py-6'>
-    <div class="w-full flex flex-col md:flex-row justify-between gap-y-8">
-        <div class="flex justify-center w-full md:w-3/12">
+    <div class="w-full flex flex-col lg:flex-row justify-between gap-y-8">
+        <div class="flex justify-center w-full sm:w-6/12 lg:w-3/12">
             <img src="/assets/growth4change_logo_long.png" alt="Growth 4 change logotype" class="object-contain w-auto h-full">
         </div>
         <ul class='flex flex-col sm:flex-row sm:justify-end sm:items-center gap-x-2 md:gap-x-3 lg:gap-x-6 xl:gap-x-12 2xl:gap-x-16 content-center'>
