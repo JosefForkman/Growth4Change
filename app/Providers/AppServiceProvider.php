79eb3fbc0e0d9296ab84d3885cf876b1fb48de1a
JosefForkman <strand.vatten@outlook.com>
2023-12-28 18:18:29 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -18,10 +19,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
+        if(env('APP_ENV') !== 'local')
+        {
+            $url->forceScheme('https');
         }
     }
 }
