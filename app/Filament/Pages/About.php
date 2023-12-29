0970db881477d4a2ead99d0fcda6709e9e928067
TGKElit <tgkelit@gmail.com>
2023-12-29 09:39:02 +0100

    Created pages tab in admin panel

--- app/Filament/Pages/About.php
+++ app/Filament/Pages/About.php
@@ -8,6 +8,8 @@ use Filament\Pages\Page;
 class About extends Page
 {
 
+    protected static ?string $navigationGroup = 'Pages';
+    
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.about';
