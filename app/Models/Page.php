b457019bb3ad172e03973d326144d0585d46ff76
JosefForkman <strand.vatten@outlook.com>
2024-01-01 21:27:17 +0100

    undone some changes

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Image\Manipulations;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class Page extends Model implements HasMedia
 {
@@ -16,6 +18,13 @@ class Page extends Model implements HasMedia
         'title',
         'structure',
     ];
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this
+            ->addMediaConversion('preview')
+            ->fit(Manipulations::FIT_CROP, 300, 300)
+            ->nonQueued();
+    }
     protected $casts = [
         'structure' => 'array',
     ];
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,12 +2,9 @@
 
 @section('content')
     <p class='font-body text-2xl'>This is the {{$page}} page</p>
-{{--    @dd($html[0])--}}
     @foreach($html as $content)
         @if($content['type'] === 'FlipCard')
-            @foreach($content['data']['Cards'] as $card)
-                <x-slide :cards="$card['data']"></x-slide>
-            @endforeach
+            <x-slide :cards="$content['data']['Cards']" :title="$content['data']['Heading']"></x-slide>
         @endif
     @endforeach
 @stop
