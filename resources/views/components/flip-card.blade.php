42022f580067b31ac558b365842cbcf640c731f8
TGKElit <tgkelit@gmail.com>
2024-01-04 12:07:27 +0100

    initial styling

--- resources/views/components/flip-card.blade.php
+++ resources/views/components/flip-card.blade.php
@@ -1,3 +1,9 @@
-<div>
-    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
+<div class="rounded-lg border-2 border-growthgreen bg-growthdarkgrey w-[500px] h-[700px] flex flex-col justify-between">
+    <img class="w-full h-1/2" src="" alt="Thumbnail Image">
+    <h3 class="text-2xl text-growthgreen uppercase m-2">Sample Title</h3>
+    <p class="text-lg text-white h-24 overflow-clip m-2"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sem purus, tempus at fermentum a, pulvinar ac lectus. Sed fermentum arcu scelerisque sapien condimentum, quis ornare eros ultricies. Sed ullamcorper in augue quis dignissim. Suspendisse vitae ligula libero. Morbi ex neque, convallis eget vulputate id, iaculis id risus. Nulla lacus dolor, consectetur in lacinia ac, vehicula eget magna. Etiam dapibus arcu diam, eget convallis mi facilisis eget.
+
+Curabitur laoreet vehicula dolor quis consectetur. Donec et aliquam dolor. Etiam molestie porttitor augue, at lobortis metus auctor feugiat. Morbi ut scelerisque purus. In ut enim leo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis libero ante, scelerisque id ante in, scelerisque iaculis elit. Nulla ornare ex libero, quis eleifend purus feugiat sed. Quisque auctor tortor nibh, vitae rutrum neque dapibus molestie. Praesent molestie feugiat sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam condimentum enim sed turpis ultrices, sed pretium dolor tincidunt. Pellentesque nec porta sapien. Sed sed fringilla mauris, in laoreet nunc. Morbi sagittis tortor metus. Morbi porttitor laoreet rutrum. </p>
+    <button class="bg-growthgreen rounded text-white p-2 m-2 w-32">READ MORE</button>
+    <!--<button class="invisible bg-growthgreen rounded text-white p-2 m-2 w-32">READ LESS</button>-->
 </div>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
     <p class='font-body text-2xl'>This is the {{$page}} page</p>
-
+    <x-flip-card/>
 @stop
