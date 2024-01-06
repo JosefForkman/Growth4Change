704bebe139f1cd4adee7478e7d009e30313a811a
JosefForkman <strand.vatten@outlook.com>
2024-01-06 16:03:58 +0100

    fix Changemakers carusel

--- resources/views/livewire/sponsor.blade.php
+++ resources/views/livewire/sponsor.blade.php
@@ -1,53 +1,20 @@
+{{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
 <section class="flex flex-col gap-y-2" aria-labelledby="carouselheading">
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
     <h2 id="carouselheading" class="text-4xl font-heading text-growthgreen xl:text-6xl">Changemakers</h2>
-    <ul class="flex gap-x-11 overflow-x-scroll h-32">
-    @foreach ($this->sponsors as $sponsor)
-    @if ($sponsor->getFirstMediaUrl())
-        <li class="h-32 flex-grow">
-            <img
-                src="{{ $sponsor->getFirstMediaUrl() }}"
-                alt="{{ $sponsor->name }}"
-                class="h-full object-cover" />
-        </li>
-        <li class="h-32 flex-grow">
-            <img
-                src="{{ $sponsor->getFirstMediaUrl() }}"
-                alt="{{ $sponsor->name }}"
-                class="h-full object-cover" />
-        </li>
-        <li class="h-32 flex-grow">
-            <img
-                src="{{ $sponsor->getFirstMediaUrl() }}"
-                alt="{{ $sponsor->name }}"
-                class="h-full object-cover" />
-        </li>
-        <li class="h-32 flex-grow">
-            <img
-                src="{{ $sponsor->getFirstMediaUrl() }}"
-                alt="{{ $sponsor->name }}"
-                class="h-full object-cover h-32" />
-        </li>
+    <ul
+        class="grid justify-items-start overflow-auto gap-6 scroll-smooth snap-x snap-mandatory"
+        style="grid-template-columns: repeat({{ $this->sponsors->count() }}, auto);"
+    >
 
-    @endif
-    @endforeach
-    </ul>
-    <ul class="flex gap-x-4 justify-center">
-        <li>
-            <button class="w-4 h-4 bg-growthdarkgrey rounded-full"></button>
-        </li>
-        <li>
-            <button class="w-4 h-4 bg-growthlightgrey rounded-full"></button>
-        </li>
-        <li>
-            <button class="w-4 h-4 bg-growthlightgrey rounded-full"></button>
-        </li>
 
-        <li>
-            <button><x-fas-chevron-left class="h-4" aria-label="Previous Item"/></button>
-        </li>
-        <li>
-            <button><x-fas-chevron-right class="h-4" aria-label="Previous Item"/></button>
-        </li>
+        @foreach ($this->sponsors as $sponsor)
+                <li class="w-80 snap-center">
+                    <img
+                        src="{{ $sponsor->getFirstMediaUrl() }}"
+                        alt="{{ $sponsor->name }}"
+                        class="object-cover w-full" />
+                </li>
+        @endforeach
     </ul>
+
 </section>
