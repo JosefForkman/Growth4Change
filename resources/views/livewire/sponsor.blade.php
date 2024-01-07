2826e9d11a1a178413f98487610cac70189fbebc
JosefForkman <strand.vatten@outlook.com>
2024-01-07 12:32:04 +0100

    center aline img

--- resources/views/livewire/sponsor.blade.php
+++ resources/views/livewire/sponsor.blade.php
@@ -2,10 +2,9 @@
 <section class="flex flex-col gap-y-2" aria-labelledby="carouselheading">
     <h2 id="carouselheading" class="text-4xl font-heading text-growthgreen xl:text-6xl">Changemakers</h2>
     <ul
-        class="grid justify-items-start overflow-auto gap-6 scroll-smooth snap-x snap-mandatory"
+        class="grid justify-items-start items-center overflow-auto gap-6 scroll-smooth snap-x snap-mandatory"
         style="grid-template-columns: repeat({{ $this->sponsors->count() }}, auto);"
     >
-
 
         @foreach ($this->sponsors as $sponsor)
                 <li class="w-80 snap-center">
