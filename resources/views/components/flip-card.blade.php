8748a266d24cbe9c5e9810a785c57660f1a8b6dc
JosefForkman <strand.vatten@outlook.com>
2024-01-10 14:30:59 +0100

    Update flip-card.blade.php

--- resources/views/components/flip-card.blade.php
+++ resources/views/components/flip-card.blade.php
@@ -1,28 +1,29 @@
 <article
     class="rounded-lg w-[500px] flex flex-col justify-between bg-growthdarkgrey"
     aria-labelledby='title'>
-    <img class="w-full h-1/2" src="{{asset('assets/Resurs-1-8.png')}}" alt="Thumbnail Image">
+    <img class="w-full h-80" src="{{asset('assets/Resurs-1-8.png')}}" alt="Thumbnail Image">
 
-    <div class='flex flex-col gap-y-4 p-4 border-4 border-t-0 border-growthgreen rounded-b-lg'>
+    <div class='flex flex-col justify-between gap-y-4 h-80 p-4 border-4 border-t-0 border-growthgreen rounded-b-lg'>
         <h3 class="text-2xl font-heading text-growthgreen md:text-3xl xl:text-4xl" id='title'>Sample Title</h3>
-        <p class="text-lg font-body text-white h-fit line-clamp-3 overflow-hidden md:text-xl xl:text-2xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            sem purus, tempus at fermentum a, pulvinar ac lectus. Sed fermentum arcu scelerisque sapien condimentum, quis
-            ornare eros ultricies. Sed ullamcorper in augue quis dignissim. Suspendisse vitae ligula libero. Morbi ex neque,
-            convallis eget vulputate id, iaculis id risus. Nulla lacus dolor, consectetur in lacinia ac, vehicula eget
-            magna. Etiam dapibus arcu diam, eget convallis mi facilisis eget.
-        </p>
-
-        <p class="hidden text-lg font-body text-white h-fit line-clamp-3 overflow-hidden md:text-xl xl:text-2xl">
-            Curabitur laoreet vehicula dolor quis consectetur. Donec et aliquam dolor. Etiam molestie porttitor augue, at
-            lobortis metus auctor feugiat. Morbi ut scelerisque purus. In ut enim leo. Orci varius natoque penatibus et
-            magnis dis parturient montes, nascetur ridiculus mus. Duis libero ante, scelerisque id ante in, scelerisque
-            iaculis elit. Nulla ornare ex libero, quis eleifend purus feugiat sed. Quisque auctor tortor nibh, vitae rutrum
-            neque dapibus molestie. Praesent molestie feugiat sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. Aliquam condimentum enim sed turpis ultrices, sed pretium dolor tincidunt. Pellentesque nec porta sapien.
-            Sed sed fringilla mauris, in laoreet nunc. Morbi sagittis tortor metus. Morbi porttitor laoreet rutrum.
-        </p>
-        <button class="bg-growthgreen rounded text-white p-2 w-32">READ MORE</button>
+        <div class="flex flex-col gap-y-4">
+            <p class="text-lg font-body text-white h-fit line-clamp-5 overflow-hidden md:text-xl xl:text-2xl">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+                sem purus, tempus at fermentum a, pulvinar ac lectus. Sed fermentum arcu scelerisque sapien condimentum, quis
+                ornare eros ultricies. Sed ullamcorper in augue quis dignissim. Suspendisse vitae ligula libero. Morbi ex neque,
+                convallis eget vulputate id, iaculis id risus. Nulla lacus dolor, consectetur in lacinia ac, vehicula eget
+                magna. Etiam dapibus arcu diam, eget convallis mi facilisis eget.
+            </p>
+            <button class="font-heading bg-growthgreen rounded text-white p-2 w-32">READ MORE</button>
+            <p class="hidden text-lg font-body text-white h-fit line-clamp-3 overflow-hidden md:text-xl xl:text-2xl">
+                Curabitur laoreet vehicula dolor quis consectetur. Donec et aliquam dolor. Etiam molestie porttitor augue, at
+                lobortis metus auctor feugiat. Morbi ut scelerisque purus. In ut enim leo. Orci varius natoque penatibus et
+                magnis dis parturient montes, nascetur ridiculus mus. Duis libero ante, scelerisque id ante in, scelerisque
+                iaculis elit. Nulla ornare ex libero, quis eleifend purus feugiat sed. Quisque auctor tortor nibh, vitae rutrum
+                neque dapibus molestie. Praesent molestie feugiat sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit. Aliquam condimentum enim sed turpis ultrices, sed pretium dolor tincidunt. Pellentesque nec porta sapien.
+                Sed sed fringilla mauris, in laoreet nunc. Morbi sagittis tortor metus. Morbi porttitor laoreet rutrum.
+            </p>
+            <button class="hidden font-heading bg-growthgreen rounded text-white p-2 w-32">READ LESS</button>
+        </div>
     </div>
-    <!--<button class="invisible bg-growthgreen rounded text-white p-2 w-32">READ LESS</button>-->
 </article>
