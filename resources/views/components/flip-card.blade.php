1d0c2f09f0590eeafd2d00abdd9a3cc0ce815dcc
JosefForkman <strand.vatten@outlook.com>
2024-01-10 14:05:00 +0100

    add some style to card

--- resources/views/components/flip-card.blade.php
+++ resources/views/components/flip-card.blade.php
@@ -1,9 +1,28 @@
-<div class="rounded-lg border-2 border-growthgreen bg-growthdarkgrey w-[500px] h-[700px] flex flex-col justify-between">
-    <img class="w-full h-1/2" src="" alt="Thumbnail Image">
-    <h3 class="text-2xl text-growthgreen uppercase m-2">Sample Title</h3>
-    <p class="text-lg text-white h-24 overflow-clip m-2"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sem purus, tempus at fermentum a, pulvinar ac lectus. Sed fermentum arcu scelerisque sapien condimentum, quis ornare eros ultricies. Sed ullamcorper in augue quis dignissim. Suspendisse vitae ligula libero. Morbi ex neque, convallis eget vulputate id, iaculis id risus. Nulla lacus dolor, consectetur in lacinia ac, vehicula eget magna. Etiam dapibus arcu diam, eget convallis mi facilisis eget.
+<article
+    class="rounded-lg w-[500px] flex flex-col justify-between bg-growthdarkgrey"
+    aria-labelledby='title'>
+    <img class="w-full h-1/2" src="{{asset('assets/Resurs-1-8.png')}}" alt="Thumbnail Image">
 
-Curabitur laoreet vehicula dolor quis consectetur. Donec et aliquam dolor. Etiam molestie porttitor augue, at lobortis metus auctor feugiat. Morbi ut scelerisque purus. In ut enim leo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis libero ante, scelerisque id ante in, scelerisque iaculis elit. Nulla ornare ex libero, quis eleifend purus feugiat sed. Quisque auctor tortor nibh, vitae rutrum neque dapibus molestie. Praesent molestie feugiat sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam condimentum enim sed turpis ultrices, sed pretium dolor tincidunt. Pellentesque nec porta sapien. Sed sed fringilla mauris, in laoreet nunc. Morbi sagittis tortor metus. Morbi porttitor laoreet rutrum. </p>
-    <button class="bg-growthgreen rounded text-white p-2 m-2 w-32">READ MORE</button>
-    <!--<button class="invisible bg-growthgreen rounded text-white p-2 m-2 w-32">READ LESS</button>-->
-</div>
+    <div class='flex flex-col gap-y-4 p-4 border-4 border-t-0 border-growthgreen rounded-b-lg'>
+        <h3 class="text-2xl font-heading text-growthgreen md:text-3xl xl:text-4xl" id='title'>Sample Title</h3>
+        <p class="text-lg font-body text-white h-fit line-clamp-3 overflow-hidden md:text-xl xl:text-2xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+            sem purus, tempus at fermentum a, pulvinar ac lectus. Sed fermentum arcu scelerisque sapien condimentum, quis
+            ornare eros ultricies. Sed ullamcorper in augue quis dignissim. Suspendisse vitae ligula libero. Morbi ex neque,
+            convallis eget vulputate id, iaculis id risus. Nulla lacus dolor, consectetur in lacinia ac, vehicula eget
+            magna. Etiam dapibus arcu diam, eget convallis mi facilisis eget.
+        </p>
+
+        <p class="hidden text-lg font-body text-white h-fit line-clamp-3 overflow-hidden md:text-xl xl:text-2xl">
+            Curabitur laoreet vehicula dolor quis consectetur. Donec et aliquam dolor. Etiam molestie porttitor augue, at
+            lobortis metus auctor feugiat. Morbi ut scelerisque purus. In ut enim leo. Orci varius natoque penatibus et
+            magnis dis parturient montes, nascetur ridiculus mus. Duis libero ante, scelerisque id ante in, scelerisque
+            iaculis elit. Nulla ornare ex libero, quis eleifend purus feugiat sed. Quisque auctor tortor nibh, vitae rutrum
+            neque dapibus molestie. Praesent molestie feugiat sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit. Aliquam condimentum enim sed turpis ultrices, sed pretium dolor tincidunt. Pellentesque nec porta sapien.
+            Sed sed fringilla mauris, in laoreet nunc. Morbi sagittis tortor metus. Morbi porttitor laoreet rutrum.
+        </p>
+        <button class="bg-growthgreen rounded text-white p-2 w-32">READ MORE</button>
+    </div>
+    <!--<button class="invisible bg-growthgreen rounded text-white p-2 w-32">READ LESS</button>-->
+</article>
