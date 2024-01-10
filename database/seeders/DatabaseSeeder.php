530ce9194f0f464cf5075132700f68dcf3899d15
JosefForkman <strand.vatten@outlook.com>
2024-01-10 09:15:00 +0100

    move seeders to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
@@ -16,11 +18,23 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
+        User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
         ]);
+        DB::table('home_pages')->insert([
+            'name' => 'About',
+        ]);
+
+        $names = ['Ecosystem', 'Food is politics', 'Resources', 'Grow with us'];
+
+        foreach ($names as $name) {
+            DB::table('pages')->insert([
+                'name' => $name,
+                'slug' => Str::slug($name),
+            ]);
+        }
     }
 }
