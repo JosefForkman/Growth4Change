a8fde32b5fdad9185713696175506ddc75b804f7
JosefForkman <strand.vatten@outlook.com>
2024-01-14 16:49:51 +0100

    add Alpine js

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{$title}}</title>
 
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.3/dist/cdn.min.js"></script>
     @filamentStyles
     @vite('resources/css/app.css')
-
 </head>
